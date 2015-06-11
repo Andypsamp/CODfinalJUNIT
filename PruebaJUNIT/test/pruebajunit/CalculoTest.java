e93a460bffa5131f9ac2f4bfc92a8d7e1a35fef7
ANDY PEÑA SAMPEDRO <ANDY PEA SAMPEDRO@ANDY>
2015-06-11 08:53:31 +0200

    finalizado el primer ejercicio con sus comentarios

--- PruebaJUNIT/test/pruebajunit/CalculoTest.java
+++ PruebaJUNIT/test/pruebajunit/CalculoTest.java
@@ -44,13 +44,13 @@ public class CalculoTest {
     @Test
     public void testObtenerMCD() {
         System.out.println("obtenerMCD");
-        int num1 =40;
-        int num2 =10;
+        int num1 =40;//num 1
+        int num2 =10;//num 2
         Calculo instance = new Calculo();
-        int expResult = 10;
+        int expResult = 10;//resul
         int result = instance.obtenerMCD(num1, num2);
         assertEquals(expResult, result);
-        System.out.println("Resultado="+expResult);
+        System.out.println("Resultado="+expResult);//visualizacion
         // TODO review the generated test code and remove the default call to fail.
        // fail("The test case is a prototype.");
     }
