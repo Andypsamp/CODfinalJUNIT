a9d19bbc4df558cfe9611e53de306351a0878d78
ANDY PEÑA SAMPEDRO <ANDY PEA SAMPEDRO@ANDY>
2015-06-11 09:56:27 +0200

    creamos una nueva clase para hacer el test de mensajeria

---
+++ PruebaJUNIT/test/pruebajunit/MensajeTest.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajunit;
+
+import mcd.Calculo;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ANDY PEÑA SAMPEDRO
+ */
+public class MensajeTest {
+    
+    public MensajeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+        @Test
+    public void testGetMensajeResultado() {
+        System.out.println("getMensajeResultado");
+        Calculo instance = new Calculo();
+        String expResult = "";
+        String result = instance.getMensajeResultado();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of obtenerMCD method, of class Calculo.
+     */
+    @Test
+    public void testObtenerMCD() {
+        System.out.println("obtenerMCD");
+        int num1 =40;//num 1
+        int num2 =10;//num 2
+        Calculo instance = new Calculo();
+        int expResult = 10;//resul
+        int result = instance.obtenerMCD(num1, num2);
+        assertEquals(expResult, result);
+        System.out.println("Resultado="+expResult);//visualizacion
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+}
