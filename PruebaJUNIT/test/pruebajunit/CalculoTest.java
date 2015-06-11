66a6a5ff272725e3ae45eb347930016e89f6444e
ANDY PEÑA SAMPEDRO <ANDY PEA SAMPEDRO@ANDY>
2015-06-11 08:46:43 +0200

    creado el test y primer commit

---
+++ PruebaJUNIT/test/pruebajunit/CalculoTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajunit;
+
+import mcd.Calculo;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ANDY PEÑA SAMPEDRO
+ */
+public class CalculoTest {
+    
+    public CalculoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of obtenerMCD method, of class Calculo.
+     */
+    @Test
+    public void testObtenerMCD() {
+        System.out.println("obtenerMCD");
+        int num1 = 0;
+        int num2 = 0;
+        Calculo instance = new Calculo();
+        int expResult = 0;
+        int result = instance.obtenerMCD(num1, num2);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+}
